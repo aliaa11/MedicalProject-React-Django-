3eef7174956991a2b5d1e6952639ec492dec0cf9
Aliaa <aliaaalihesh@gmail.com>
2025-03-15 00:54:00 +0200

    HomePage,Exams,Teacher,student,class,class-student

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from './home/home.component';
 import { ExamsComponent } from './exams/exams.component';
 import { StudentComponent } from './student/student.component';
 import { TeacherComponent } from './teacher/teacher.component';
+import { ClassCardComponent } from './class-card/class-card.component';
 
 export const routes: Routes = [
     {
@@ -17,4 +18,5 @@ export const routes: Routes = [
     { path: 'students/:query', component: StudentComponent }, 
     { path: 'teachers', component: TeacherComponent },
     { path: 'teachers/:query', component: TeacherComponent }, 
+    {path: 'classes',component: ClassCardComponent}
 ];
